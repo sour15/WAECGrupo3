3e71005f165afb3e019e3a5e868662c914e25e25
sour15 <ashleeagres56@gmail.com>
2024-05-24 15:14:57 -0500

    Iniciando Proyecto

---
+++ src/main/java/com/example/WAEC2Grupo3/Waec2Grupo3Application.java
@@ -0,0 +1,13 @@
+package com.example.WAEC2Grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2Grupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2Grupo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WAEC2Grupo3/Waec2Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WAEC2Grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2Grupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
